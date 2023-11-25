711996227702bbd3a71da71ee2e15b3384331c51
iamgian2001 <ggogamer60@gmail.com>
2023-11-25 22:34:56 +0530

    merging styles pt1

--- app/views/admin/admin_dashboard.php
+++ app/views/admin/admin_dashboard.php
@@ -2,20 +2,31 @@
 <html lang="en">
 <head>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="widtd=device-widtd, initial-scale=1.0">
     <title>Manage Admin</title>
-    <link rel="stylesheet" type="text/css" href="css/com.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" type="text/css" href="../../../public/css/com.css?v=<?php echo time(); ?>">
 
 </head>
 <body>
 
-<div class="outer">
-    <div class="container">
+<div class="container">
         <?php include_once('sidemenu.php') ?>
-    </div>
+        <div class ="main">
+            <div class = "topbar">
+                <div class = "toggle">
+                    <ion-icon name="menu-outline"></ion-icon>
+                </div>
+                <div class = "user">
+                    <ion-icon name="notifications-circle-outline"></ion-icon>
+                    <span>Hamsayini</span>
+                    <ion-icon name="person-circle-outline"></ion-icon>
+                    
+                </div>
 
-   <div class="content">
-    <div class="notification">
+            </div>
+
+    <div class="content">
+        <div class="notification">
                     <div class="notibutton">
                         <a href="">
                             <div>
@@ -55,84 +66,103 @@
                     </div>  
                          
         </div>
-        <table>
-            <caption>Advertisement report<caption>
-                <tr>
-                    <th>Company name</th>
-                    <th>job position</th>
-                    <th>interns</th>
-                </tr>
+    <div>
+    
 
-                <tr>
-                    <td>99x</td>
-                    <td>Network Enginner</td>
-                    <td>6</td>
-                </tr>
+    <div class="details">
+        <div class="companyList">
+                <div class = "cardHeader">
+                    <h2>Advertisment Report</h2>
+                    <!-- <a href="#" class="btn">View All</a>  -->
+                </div>
+            <table>
+                <thead>
+                    <tr>
+                        <td>Company name</td>
+                        <td>job position</td>
+                        <td>interns</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>99x</td>
+                        <td>Network Enginner</td>
+                        <td>6</td>
+                    </tr>
 
-                <tr>
-                    <td>WSO2</td>
-                    <td>Cyber secruity</td>
-                    <td>4</td>
-                </tr>
+                    <tr>
+                        <td>WSO2</td>
+                        <td>Cyber secruity</td>
+                        <td>4</td>
+                    </tr>
 
-                <tr>
-                    <td>Virtusa</td>
-                    <td>IT officer</td>
-                    <td>1</td>
-                </tr>
+                    <tr>
+                        <td>Virtusa</td>
+                        <td>IT officer</td>
+                        <td>1</td>
+                    </tr>
 
-                <tr>
-                    <td>CodeGen</td>
-                    <td>Software Enginner</td>
-                    <td>12</td>
-                </tr>
-                    <td class="td-left" colspan="3">TOTAL = 23</td>
-                <tr>
+                    <tr>
+                        <td>CodeGen</td>
+                        <td>Software Enginner</td>
+                        <td>12</td>
+                    </tr>
+                        <td class="td-left" colspan="3">TOTAL = 23</td>
+                    <tr>
+                </tbody>
+            </table>
+        </div>
 
-        </table>
+        <div class="companyList">
+                <div class = "cardHeader">
+                    <h2>Registration report</h2>
+                    <!-- <a href="#" class="btn">View All</a>  -->
+                </div>
 
-        <table>
-            <caption>Registration report<caption>
-                <tr>
-                    <th>Company</th>
-                    <th>Contact Name</th>
-                    <th>Contact</th>
-                    <th>email</th>
-                </tr>
+            <table>
+                <thead>
+                    <tr>
+                        <td>Company</td>
+                        <td>Contact Name</td>
+                        <td>Contact</td>
+                        <td>email</td>
+                    </tr>
+                </thead>
 
-                <tr>
-                    <td>99x</td>
-                    <td>Ruwan</td>
-                    <td>077-8982122</td>
-                    <td>Ruwan@gmail.com</td>
-                </tr>
+                <tbody>
 
-                <tr>
-                    <td>WSO2</td>
-                    <td>Ajith</td>
-                    <td>077-8982122</td>
-                    <td>Ajith@gmail.com</td>
-                </tr>
+                    <tr>
+                        <td>99x</td>
+                        <td>Ruwan</td>
+                        <td>077-8982122</td>
+                        <td>Ruwan@gmail.com</td>
+                    </tr>
 
-                <tr>
-                    <td>Virtusa</td>
-                    <td>fernando</td>
-                    <td>077-8982122</td>
-                    <td>fernando@gmail.com</td>
-                </tr>
+                    <tr>
+                        <td>WSO2</td>
+                        <td>Ajitd</td>
+                        <td>077-8982122</td>
+                        <td>Ajitd@gmail.com</td>
+                    </tr>
 
-                <tr>
-                    <td>CodeGen</td>
-                    <td>kanith</td>
-                    <td>077-8982122</td>
-                    <td>kanith@gmail.com</td>
+                    <tr>
+                        <td>Virtusa</td>
+                        <td>fernando</td>
+                        <td>077-8982122</td>
+                        <td>fernando@gmail.com</td>
+                    </tr>
 
-        </table>
+                    <tr>
+                        <td>CodeGen</td>
+                        <td>kanitd</td>
+                        <td>077-8982122</td>
+                        <td>kanitd@gmail.com</td>
+                </tbody>
+
+            </table>
+        </div>
     </div>
     
-    <div class="userhover">
-        <?php include ('logpopup.php')?>
-    </div>
 
 </div>   
     
--- app/views/admin/complaints.php
+++ app/views/admin/complaints.php
@@ -2,141 +2,141 @@
 <html lang="en">
 <head>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="widtd=device-widtd, initial-scale=1.0">
     <title>Manage Admin</title>
-    <link rel="stylesheet" type="text/css" href="css/com.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" type="text/css" href="../../../public/css/com.css?v=<?php echo time(); ?>">
 
 </head>
 <body>
 
-<div class="outer">
-    <div class="container">
+<div class="container">
         <?php include_once('sidemenu.php') ?>
-   </div>
-
-
-   
-
-    <div class="content">
-        <div class="notification">
-                <div class="notibutton">
-                        <a>
-                            <div>
-                                <div>8</div>
-                                <div>Complaints to Review</div>
-                            </div>
-                        </a>
-                        <div class="iconBx">
-                            <ion-icon name="alert-circle" size="large"></ion-icon>
-                        </div>
-                            
+        <div class ="main">
+            <div class = "topbar">
+                <div class = "toggle">
+                    <ion-icon name="menu-outline"></ion-icon>
                 </div>
+                <div class = "user">
+                    <ion-icon name="notifications-circle-outline"></ion-icon>
+                    <span>Hamsayini</span>
+                    <ion-icon name="person-circle-outline"></ion-icon>
+                    
+                </div>
+
+            </div>
+
+   <div class="details">
+        <div class="companyList">
+                <div class = "cardHeader">
+                    <h2>Complaints</h2>
+                    <!-- <a href="#" class="btn">View All</a>  -->
+                </div>
+
+            <table>
+                <thead>
+                    <tr>
+                        <td>Review-status</td>
+                        <td>Student Name</td>
+                        <td>Email</td>
+                        <td>Contact</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Not reviewed</td>
+                        <td>Nirmal</td>
+                        <td>hamsa@gmail.com</td>
+                        <td>0763421345</td>
+                        <td><a href="#" span class = "view"></span>View</td>
+                    </tr>
+
+
+                    <tr>
+                        <td>Not reviewed</td>
+                        <td>Kasun</td>
+                        <td>nufdha@gmail.com</td>
+                        <td>0713245678</td>
+                        <td><a href="#" span class = "view"></span>View</td>
+                    </tr>
+
+                    <tr>
+                        <td>Not reviewed</td>
+                        <td>Kaalik</td>
+                        <td>shama@gmail.com</td>
+                        <td>1230543721</td>
+                        <td><a href="#" span class = "view"></span>View</td>
+                    </tr>
+
+                    
+
+                    <tr>
+                        <td>Not reviewed</td>
+                        <td>Gopika</td>
+                        <td>shama@gmail.com</td>
+                        <td>0734576123</td>
+                        <td><a href="#" span class = "view"></span>View</td>
+                    </tr>
+
+                    <tr>
+                        <td>Not reviewed</td>
+                        <td>Salama</td>
+                        <td>nufdha@gmail.com</td>
+                        <td>0754329123</td>
+                        <td><a href="#" span class = "view"></span>View</td>
+                    </tr>
+
+                    <tr>
+                        <td>Not reviewed</td>
+                        <td>Saktdi</td>
+                        <td>gawesh@gmail.com</td>
+                        <td>0987655455</td>
+                    <td><a href="#" span class = "view"></span>View</td>
+                    </tr>
+
+
+                    <tr>
+                        <td>Not reviewed</td>
+                        <td>Khalid</td>
+                        <td>hamsa@gmail.com</td>
+                        <td>0876532123</td>
+                    <td><a href="#" span class = "view"></span>View</td>
+                    </tr>
+
+                    <tr>
+                        <td>Not reviewed</td>
+                        <td>Nirush</td>
+                        <td>gawesh@gmail.com</td>
+                        <td>0764532123</td>
+                    <td><a href="#" span class = "view"></span>View</td>
+                    </tr>
+
+                    <tr>
+                        <td>Reviewed</td>
+                        <td>Vikash</td>
+                        <td>shama@gmail.com</td>
+                        <td>0874532145</td>
+                    <td><a href="#" span class = "view"></span>View</td>
+                    </tr>
+
+                    <tr>
+                        <td>Reviewed</td>
+                        <td>Chamal</td>
+                        <td>hamsa@gmail.com</td>
+                        <td>0763421567</td>
+                    <td><a href="#" span class = "view"></span>View</td>
+                    </tr>
+
+                </tbody>
+            </table>
         </div>
-        <table>
-                <caption>Complaints</caption>
-                <tr>
-                    <th>Review-status</th>
-                    <th>Student Name</th>
-                    <th>Email</th>
-                    <th colspan="2">Contact</th>
-                </tr>
-
-                <tr>
-                    <td>Not reviewed</td>
-                    <td>Nirmal</td>
-                    <td>hamsa@gmail.com</td>
-                    <td>0763421345</td>
-                   <a href="" span class = "view"><td>View</td></a>
-                </tr>
-                </tr>
-
-                <tr>
-                    <td>Not reviewed</td>
-                    <td>Kasun</td>
-                    <td>nufdha@gmail.com</td>
-                    <td>0713245678</td>
-                    <a href="" span class = "view"><td>View</td></a>
-                </tr>
-
-                <tr>
-                    <td>Not reviewed</td>
-                    <td>Kaalik</td>
-                    <td>shama@gmail.com</td>
-                    <td>1230543721</td>
-                    <a href="" span class = "view"><td>View</td></a>
-                </tr>
-
-                
-
-                <tr>
-                    <td>Not reviewed</td>
-                    <td>Gopika</td>
-                    <td>shama@gmail.com</td>
-                    <td>0734576123</td>
-                    <a href="" span class = "view"><td>View</td></a>
-                </tr>
-
-                <tr>
-                    <td>Not reviewed</td>
-                    <td>Salama</td>
-                    <td>nufdha@gmail.com</td>
-                    <td>0754329123</td>
-                    <a href="" span class = "view"><td>View</td></a>
-                </tr>
-
-                <tr>
-                    <td>Not reviewed</td>
-                    <td>Sakthi</td>
-                    <td>gawesh@gmail.com</td>
-                    <td>0987655455</td>
-                   <a href="" span class = "view"><td>View</td></a>
-                </tr>
-                </tr>
-
-                <tr>
-                    <td>Not reviewed</td>
-                    <td>Khalid</td>
-                    <td>hamsa@gmail.com</td>
-                    <td>0876532123</td>
-                   <a href="" span class = "view"><td>View</td></a>
-                </tr>
-                </tr>
-
-                <tr>
-                    <td>Not reviewed</td>
-                    <td>Nirush</td>
-                    <td>gawesh@gmail.com</td>
-                    <td>0764532123</td>
-                   <a href="" span class = "view"><td>View</td></a>
-                </tr>
-                </tr>
-
-                <tr>
-                    <td>Reviewed</td>
-                    <td>Vikash</td>
-                    <td>shama@gmail.com</td>
-                    <td>0874532145</td>
-                   <a href="" span class = "view"><td>View</td></a>
-                </tr>
-                </tr>
-
-                <tr>
-                    <td>Reviewed</td>
-                    <td>Chamal</td>
-                    <td>hamsa@gmail.com</td>
-                    <td>0763421567</td>
-                   <a href="" span class = "view"><td>View</td></a>
-                </tr>
-                </tr>
-        </table>
     </div>
+
     <div class="userhover">
         <?php include ('logpopup.php')?>
     </div>
 
 </div>
     
-</div>
    
 </body>
 </html>
